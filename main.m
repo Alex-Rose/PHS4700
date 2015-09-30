f9a828d888e8f83fba8b13468db89885406a6f67
Alexandre Rose <alexandre.rose.0@gmail.com>
2015-09-30 00:31:53 -0400

    fix rotation

--- main.m
+++ main.m
@@ -1,46 +1,65 @@
 clear;  %clear la mémoire
 clc;    %clear le texte de la console
 
-Declarations
+Declarations1
+
+% Simuler l'inclinaison de 10deg
+incline = true;
 
 % Calcul du centre de masse global de l'objet
 cdms = [ jambeGauche.CentreDeMasse jambeDroite.CentreDeMasse tronc.CentreDeMasse cou.CentreDeMasse brasGauche.CentreDeMasse brasDroit.CentreDeMasse tete.CentreDeMasse ];
 masses = [ jambeGauche.Masse jambeDroite.Masse tronc.Masse cou.Masse brasGauche.Masse brasDroit.Masse tete.Masse ];
 centreMasseGlobal = CentreDeMasse(cdms, masses);
 
-disp('Centre de masse du patineur')
-disp(centreMasseGlobal')
+centreMasseIncline = centreMasseGlobal;
 
-centreMasseToBePrinted = centreMasseGlobal;
-% Si incliné
-rot = Rotation(0,-pi/18,0);
-centreMasseToBePrinted = (rot*centreMasseGlobal);
+if (incline)
+    rot = Rotation(0,-pi/18,0);
+    centreMasseIncline = (rot*centreMasseGlobal);
+end
+
+disp('Question a)')
+disp('Centre de masse du patineur = ')
+disp(centreMasseIncline)
+
 
 % Application de la force
 pointForce = [0 tete.Rayon jambeGauche.Longueur + tronc.Longueur + cou.Longueur + tete.Rayon]';
 
-% Si incliné
-% pointForce = (rot*pointForce)'*inv(rot);
+if (incline)
+    pointForce = (rot*pointForce);
+end
 
 vecteurForce = [0 -200 0]';
 
-% Si incliné
-% vecteurForce = (rot*vecteurForce)'*inv(rot)
+momentForce = MomentForce(pointForce, centreMasseIncline, vecteurForce);
 
-momentForce = MomentForce(pointForce, centreMasseGlobal, vecteurForce);
-
+% Moment d'inertie
 momentInertie = jambeGauche.MomentInertie(centreMasseGlobal);
 momentInertie = momentInertie + jambeDroite.MomentInertie(centreMasseGlobal);
 momentInertie = momentInertie + tronc.MomentInertie(centreMasseGlobal);
 momentInertie = momentInertie + cou.MomentInertie(centreMasseGlobal);
 momentInertie = momentInertie + brasDroit.MomentInertie(centreMasseGlobal);
 momentInertie = momentInertie + brasGauche.MomentInertie(centreMasseGlobal);
-momentInertie = momentInertie + tete.MomentInertie(centreMasseGlobal)
+momentInertie = momentInertie + tete.MomentInertie(centreMasseGlobal);
 
-% Si incliné
-%momentInertie = rot*momentInertie*inv(rot)
+if (incline)
+    momentInertie = rot*momentInertie*inv(rot);
+end
 
-acceleration = Acceleration(pointForce, vecteurForce, centreMasseGlobal, momentInertie, [0 0 0]')
+disp('Question b)')
+disp('Moment inertie = ')
+disp(momentInertie)
 
-acceleration = Acceleration(pointForce, vecteurForce, centreMasseGlobal, momentInertie, [0 0 10]')
+% Acceleration
+disp('Question c)')
+acceleration = Acceleration(pointForce, vecteurForce, centreMasseIncline, momentInertie, [0 0 0]')
+
+vi = [0 0 10]';
+if (incline)
+    vi = rot * vi;
+end
+
+disp('Question d)')
+acceleration = Acceleration(pointForce, vecteurForce, centreMasseIncline, momentInertie, vi)
 
