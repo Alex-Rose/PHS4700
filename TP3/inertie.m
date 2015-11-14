6d5809eddce3faa096a7b4c075a25c0e40666e15
unknown <angosa@L4708-06.gigl.polymtl.ca>
2015-11-14 16:26:59 -0500

    vitesses apres collision

---
+++ TP3/inertie.m
@@ -0,0 +1,20 @@
+
+function [ moment ] = inertie( masse, rayon, hauteur, isSphere )
+    moment = [1 0 0 ; 0 1 0; 0 0 1];
+
+    if (isSphere)
+        % Sphere
+        %Ic,xx = Ic,yy = Ic,zz = (2m/5)(rayon^2)
+        moment(1,1) = (2 * masse / 5) * power(rayon, 2);
+        moment(2,2) = moment(1,1);
+        moment(3,3) = moment(1,1);
+    else
+        % Cylindre
+        %Ic,zz = (m)*(r^2)
+        moment(3,3) = masse * power(rayon,2);
+
+        %Ic,xx = Ic,yy = (m/2)*(r^2) + (m/12)*(l^2)
+        moment(1,1) = (masse / 2) * power(rayon, 2) + (masse / 12) * (power(hauteur, 2));
+        moment(2,2) = moment(1,1);
+    end
+end
