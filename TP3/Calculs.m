9f0b462ebc3c839105b4eca3380f02317b452245
Alexandre Rose <alexandre.rose.0@gmail.com>
2015-11-15 11:26:08 -0500

    distance collision

--- TP3/Calculs.m
+++ TP3/Calculs.m
@@ -7,8 +7,13 @@ classdef Calculs
        function f = visqueuxBalle(obj, v)
            f = - obj.k * obj.A * v;
        end
+       
        function v = vitesse(vi, a, dt)
            v = [vi(1) + a(1) * dt vi(2) + a(2) * dt vi(3) + a(3) * dt];
+       end
+       
+       function v = vecteurEntrePoints(p1, p2) 
+          v = p2 - p1;
        end
    end
 end
