66350d0a2cee21e8be0d5652ab9e08a13725188e
antoine <gosselin.antoine@gmail.com>
2015-11-18 13:16:58 -0500

    Rapport: resultats, fin description logiciel, equation collisions Ajout tableaux resultats Correction orientation vecteur dans l'output

--- TP3/main.m
+++ TP3/main.m
@@ -5,7 +5,7 @@ clc;    %clear le texte de la console
 disp('TP3')
 
 
-[ Dev vbaf vbof rba rbo tballe ] = Devoir3([ 0 0 0 ]', [ 6.85 0 6.85 ]', 0.66);
+%[ Dev vbaf vbof rba rbo tballe ] = Devoir3([ 0 0 0 ]', [ 6.85 0 6.85 ]', 0.66);
 %[ Dev vbaf vbof rba rbo tballe ] = Devoir3([ 0 0 0 ]', [ 28 0.5 10 ]', 1.1);
 %[ Dev vbaf vbof rba rbo tballe ] = Devoir3([ 0 2.3 0 ]', [ 6.85 0 6.85 ]', 0.66);
-%[ Dev vbaf vbof rba rbo tballe ] = Devoir3([ 0 2.3 0 ]', [ 28 0.5 10 ]', 1.1);
+[ Dev vbaf vbof rba rbo tballe ] = Devoir3([ 0 2.3 0 ]', [ 28 0.5 10 ]', 1.1);
