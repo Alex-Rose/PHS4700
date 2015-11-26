b2070ace9a716ccc06cbdae7efc774ac161b1239
Alexandre Rose <alexandre.rose.0@gmail.com>
2015-11-26 18:01:27 -0500

    Simulation loop

--- TP4/main.m
+++ TP4/main.m
@@ -6,7 +6,7 @@ disp('TP4')
 
 % limits = [1 1; -1 1; 0 2];
 % p = Plan([1 0 0]', [1 0 0]', limits);
-d = Droite([2 0 1]', [-10 0 0]');
+d = Droite([2 0 0]', [-10 3.5 14]');
 
 % [bool, pt] = Calculs.Collision(d, p);
 
@@ -16,62 +16,95 @@ d = Droite([2 0 1]', [-10 0 0]');
 
 [obs, blocTrans, blocColors] = Declarations([-10 -10  15], 1, 1.5);
 dedans = false;
-for i = 1:size(blocTrans.Plans(:))
-    [bool, pt] = Calculs.Collision(d, blocTrans.Plans(i));
-    if bool
-        if dedans == false
-            % Si le produit scalaire est positif, le point des face au plan
-            if dot([d.Point;1], blocTrans.Plans(i).Param) >= 0
-                Calculs.Refraction(d.u, blocTrans.Plans(i).n, 1, 1.5) 
-            end
-        else
-            if dot([d.Point;1], blocTrans.Plans(i).Param) <= 0
-                Calculs.Refraction(d.u, blocTrans.Plans(i).n*-1, 1, 1.5) 
+keep = true;
+collision = false;
+failsafe = 0;
+while keep && collision == false && failsafe < 1000;
+    if dedans == false
+        for i = 1:size(blocTrans.Plans(:))
+            [bool, pt] = Calculs.Collision(d, blocTrans.Plans(i));
+            
+            if bool
+                 % Si le produit scalaire est positif, le point est face au plan
+                if dot([d.Point;1], blocTrans.Plans(i).Param) >= 0
+                    s = Calculs.Refraction(d.u, blocTrans.Plans(i).n, 1, 1.5);
+                    if norm(s) == 0
+                       s = Calculs.Reflexion(d.u, blocTrans.Plans(i).n);
+                    else
+                        dedans = true;
+                    end
+                    d = Droite(s, pt);
+                end
             end
         end
+    else
+        % Le rayon est dedans
+        collision = false;
+        
+        % verifie collision avec le bloc de couleur
+        for i = 1:size(blocColors.Plans(:))
+            [bool, pt] = Calculs.Collision(d, blocColors.Plans(i));
+            
+            if bool
+                 % Si le produit scalaire est positif, le point est face au plan
+                if dot([d.Point;1], blocColors.Plans(i).Param) >= 0
+                    % ON A UNE COLLISION
+                    % PRENDRE LA COULEUR
+                    keep = false;
+                    collision = true;
+                    disp('Couleur ! ')
+                    disp (blocColors.GetCouleur(i));
+                    break;
+                end
+            end
+        end
+        
+        % Il n'y a pas eu de collision avec le bloc de couleur
+        if collision == false
+            for i = 1:size(blocTrans.Plans(:))
+                [bool, pt] = Calculs.Collision(d, blocTrans.Plans(i));
+            
+                if bool
+                    % Si le produit scalaire est negatif, le point est derriere le plan
+                    if dot([d.Point;1], blocTrans.Plans(i).Param) <= 0
+                        s = Calculs.Refraction(d.u, blocTrans.Plans(i).n, 1, 1.5);
+                        if norm(s) == 0
+                            s = Calculs.Reflexion(d.u, blocTrans.Plans(i).n);
+                            d = Droite(s, pt);
+                        else
+                            % Le rayon quitte le bloc. on l'elimine
+                            keep = false;
+                            dedans = false;
+                        end
+                    end
+                end
+            end % Fin de toutes les faces du bloc transparent
+        end
     end
+    failsafe = failsafe + 1;
 end
 
-file = fopen('out.bmp', 'w');
-
-x = 16;
-y = 16;
-
-BM = [ hex2dec('42') hex2dec('4D')];
-size = x * y + hex2dec('436');
-reserved = [ 0 0 0 0 ];
-offset = [ hex2dec('36') hex2dec('04') 0 0 ];
-dibsize = [hex2dec('28') 0 0 0];
-x;
-y;
-nplanes = [1 0];
-colors256_8bits = [ 8 0 ];
-compression = [0 0 0 0];
-imgSize = x * y;
-unused = zeros(16);
-
-h = fopen('header.bin');
-hbytes = fread(h);
-fclose(h);
+disp('end');
+%         if bool
+%             if dedans == false
+%                 Si le produit scalaire est positif, le point des face au plan
+%                 if dot([d.Point;1], blocTrans.Plans(i).Param) >= 0
+%                     Calculs.Refraction(d.u, blocTrans.Plans(i).n, 1, 1.5) 
+%                 end
+%             else
+%                 if dot([d.Point;1], blocTrans.Plans(i).Param) <= 0
+%                     Calculs.Refraction(d.u, blocTrans.Plans(i).n*-1, 1, 1.5) 
+%                 end
+%             end
+%         else
+%             keep = false;
+%         end
+%     end
+% end
 
 data = [];
-for i = 1:x*y
-	data = [data 79];
+for i = 1:16*16
+    data = [data 79];
 end
 
-fwrite(file, BM);
-fwrite(file, size, 'integer*4');
-fwrite(file, reserved);
-fwrite(file, offset);
-fwrite(file, dibsize);
-fwrite(file, x, 'integer*4');
-fwrite(file, y, 'integer*4');
-fwrite(file, nplanes);
-fwrite(file, colors256_8bits);
-fwrite(file, compression);
-fwrite(file, imgSize, 'integer*4');
-fwrite(file, unused(1,:));
-fwrite(file, hbytes);
-fwrite(file, data);
-
-fclose(file);
+outputBmp(16, 16, data);
